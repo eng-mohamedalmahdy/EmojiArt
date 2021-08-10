591077246eb282b40273c62ef93e180ca0a2adcc
Mohamed Almahdy <engmohamedalmahdy@gmail.com>
2021-08-10 16:38:25 +0200

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by NoobMaster69 on 10/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
